5548e62b716ca2b1a98c05b0653462ad88664688
lilchicky <thelilchicken01@gmail.com>
2023-01-07 22:18:19 -0700

    animation updates

--- Test Mod/src/main/java/com/gmail/thelilchicken01/tff/entity/custom/ReetleQueenEntity.java
+++ Test Mod/src/main/java/com/gmail/thelilchicken01/tff/entity/custom/ReetleQueenEntity.java
@@ -23,6 +23,7 @@ import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -118,11 +119,28 @@ public class ReetleQueenEntity extends Monster implements IAnimatable {
 		return PlayState.CONTINUE;
 		
 	}
+	
+	private PlayState attackPredicate(AnimationEvent event) {
+		
+		if (this.swinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
+			
+			event.getController().markNeedsReload();
+			
+			event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.reetle_queen.attack", false));
+			
+			this.swinging = false;
+			
+		}
+		
+		return PlayState.CONTINUE;
+		
+	}
 
 	@Override
 	public void registerControllers(AnimationData data) {
 		
 		data.addAnimationController(new AnimationController(this, "controller", 0, this::predicate));
+		data.addAnimationController(new AnimationController(this, "attackcontroller", 0, this::attackPredicate));
 		
 	}
 
