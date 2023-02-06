f4df8c023630d3f61bbe09fe36392749d6d74257
lilchicky <thelilchicken01@gmail.com>
2023-02-06 03:12:30 -0700

    reetle reagents werent dropping

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/armor/bansheeArmor/BansheeBoots.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/armor/bansheeArmor/BansheeBoots.java
@@ -41,6 +41,9 @@ public class BansheeBoots extends ArmorItem {
 	public final Lazy<Multimap<Attribute, AttributeModifier>> LAZY = Lazy.of(() ->  {    
     	ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder(); 
     	
+    	builder.put(Attributes.MAX_HEALTH, new AttributeModifier(UUID.randomUUID(), 
+	    		"max_health", ModArmorMaterial.BANSHEE.getDefenseForSlot(EquipmentSlot.FEET) + 4, AttributeModifier.Operation.ADDITION));
+    	
     	builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(UUID.randomUUID(), 
 	    		"move_speed", 0.25, AttributeModifier.Operation.MULTIPLY_BASE));
 	    
