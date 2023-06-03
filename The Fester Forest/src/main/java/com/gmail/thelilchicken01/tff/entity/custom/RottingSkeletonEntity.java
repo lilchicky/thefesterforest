b4421e1ac631596f642abdb39d676a5ca08e130a
lilchicky <thelilchicken01@gmail.com>
2023-06-03 02:46:13 -0600

    removed ore veins from the fester forest
    no more granite D:

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/entity/custom/RottingSkeletonEntity.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/entity/custom/RottingSkeletonEntity.java
@@ -1,9 +1,13 @@
 package com.gmail.thelilchicken01.tff.entity.custom;
 
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
