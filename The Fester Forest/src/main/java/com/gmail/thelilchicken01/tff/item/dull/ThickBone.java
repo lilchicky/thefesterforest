88b0cc17e0a6b5b33134da79f5b138f7b4f3872d
lilchicky <thelilchicken01@gmail.com>
2023-06-08 10:00:25 -0600

    thick bone lore

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/dull/ThickBone.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/dull/ThickBone.java
@@ -1,15 +1,26 @@
 package com.gmail.thelilchicken01.tff.item.dull;
 
+import java.util.List;
+
 import com.gmail.thelilchicken01.tff.TheFesterForest;
 import com.gmail.thelilchicken01.tff.capability.SwimHandler;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 public class ThickBone extends Item implements ICurioItem {
+	
+	private String[] drops = {"Seathrown Skeleton", "Fester Forest Loot Chests"};
 
 	public ThickBone() {
 		super(new Properties().stacksTo(1).tab(TheFesterForest.TFF_TAB));
@@ -21,7 +32,6 @@ public class ThickBone extends Item implements ICurioItem {
 		if (slotContext.entity() instanceof ServerPlayer player) {
 			slotContext.entity().getCapability(SwimHandler.CAPABILITY).ifPresent(
 					handler -> {
-						System.out.println("Sinking set to True");
 						handler.setSinking(true);
 						handler.syncSinking(player);
 					}
@@ -41,6 +51,24 @@ public class ThickBone extends Item implements ICurioItem {
 				);
 			
 		}
+	}
+	
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack stack, Level world, List<Component> lore, TooltipFlag flag) {
+		
+		lore.add(new TextComponent("Dull").withStyle(ChatFormatting.DARK_AQUA).withStyle(ChatFormatting.BOLD));
+		lore.add(new TextComponent(""));
+		lore.add(new TextComponent("A rather thick bone of mysterious origin.").withStyle(ChatFormatting.GRAY));
+		lore.add(new TextComponent("Makes you so heavy you cannot float in water.").withStyle(ChatFormatting.GRAY));
+		lore.add(new TextComponent(""));
+		lore.add(new TextComponent("Drops From:").withStyle(ChatFormatting.LIGHT_PURPLE));
+		for (int x = 0; x < drops.length; x++) {
+			lore.add(new TextComponent(drops[x]).withStyle(ChatFormatting.LIGHT_PURPLE));
+		}
+		lore.add(new TextComponent(""));
+		
+		super.appendHoverText(stack, world, lore, flag);
 	}
 
 }
