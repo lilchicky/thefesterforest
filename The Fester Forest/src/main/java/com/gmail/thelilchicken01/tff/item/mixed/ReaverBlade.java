c86250b5cd4f6445c59fa08b9d5516343e29f124
lilchicky <thelilchicken01@gmail.com>
2023-06-25 23:07:20 -0600

    reaver blade scales with banshee armor now

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/mixed/ReaverBlade.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/mixed/ReaverBlade.java
@@ -1,7 +1,9 @@
-package com.gmail.thelilchicken01.tff.item.melee;
+package com.gmail.thelilchicken01.tff.item.mixed;
 
 import java.util.List;
 
+import com.gmail.thelilchicken01.tff.item.armor.ArmorSets;
+import com.gmail.thelilchicken01.tff.item.armor.SetCount;
 import com.gmail.thelilchicken01.tff.item.item.EffectsUtil;
 
 import net.minecraft.ChatFormatting;
@@ -10,6 +12,7 @@ import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
@@ -21,6 +24,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class ReaverBlade extends SwordItem {
 	
 	private String[] drops = {"Deep Reaver", "Fester Forest Loot Chests"};
+	
+	private int strength;
 
 	public ReaverBlade(Tier tier, int damage, float aspeed, Properties properties) {
 		super(tier, damage, aspeed, properties);
@@ -30,7 +35,18 @@ public class ReaverBlade extends SwordItem {
 	public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		
 		EffectsUtil effect = EffectsUtil.getRandomEffect();
-		int strength = (int) (Math.random() * 6);
+		
+		if (attacker instanceof Player) {
+			if (ArmorSets.BANSHEE.getArmorSet((Player) attacker) == SetCount.TWO) {
+				strength = ((int) (Math.random() * 6)) + 4;
+			}
+			else if (ArmorSets.BANSHEE.getArmorSet((Player) attacker) == SetCount.FOUR) {
+				strength = ((int) (Math.random() * 6)) + 8;
+			}
+			else {
+				strength = (int) (Math.random() * 6);
+			}
+		}
 		
 		target.addEffect(new MobEffectInstance(effect.getEffect(), effect.getEffectDuration() * 20, strength));
 		
@@ -45,7 +61,7 @@ public class ReaverBlade extends SwordItem {
 	@OnlyIn(Dist.CLIENT)
 	public void appendHoverText(ItemStack stack, Level world, List<Component> lore, TooltipFlag flag) {
 		
-		lore.add(new TextComponent("Melee").withStyle(ChatFormatting.DARK_AQUA).withStyle(ChatFormatting.BOLD));
+		lore.add(new TextComponent("Magic, Melee").withStyle(ChatFormatting.DARK_AQUA).withStyle(ChatFormatting.BOLD));
 		lore.add(new TextComponent(""));
 		lore.add(new TextComponent("The massive tooth of a Deep Reaver. Hitting a target").withStyle(ChatFormatting.GRAY));
 		lore.add(new TextComponent("applies a random effect to both you and the target.").withStyle(ChatFormatting.GRAY));
