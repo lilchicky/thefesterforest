ba669dbe9f6149210f1c119a791012d7d7329e0b
lilchicky <thelilchicken01@gmail.com>
2023-09-23 00:00:34 -0600

    made ancient weapons look more "ancient"

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/init/ItemInit.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/init/ItemInit.java
@@ -462,7 +462,7 @@ public class ItemInit {
 	
 	//Ancient Longsword
 	public static final RegistryObject<Item> ANCIENT_LONGSWORD = register("ancient_longsword", 
-			() -> new AncientLongsword(ModTiers.METAL, 11, -1.4f, new Properties().tab(TheFesterForest.TFF_TAB).durability(10)));
+			() -> new AncientLongsword(ModTiers.METAL, 11, -2.4f, new Properties().tab(TheFesterForest.TFF_TAB).durability(10)));
 	
 	/*
 	 * 
