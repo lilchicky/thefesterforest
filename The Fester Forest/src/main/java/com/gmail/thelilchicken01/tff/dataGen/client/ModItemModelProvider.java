9e21f0f6c8e2b9a5a7032037c6807d18a8aa88b1
lilchicky <thelilchicken01@gmail.com>
2023-09-22 18:22:46 -0600

    manuall provided leaves models

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/dataGen/client/ModItemModelProvider.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/dataGen/client/ModItemModelProvider.java
@@ -73,7 +73,6 @@ public class ModItemModelProvider extends ItemModelProvider {
 		simpleBlockItem(BlockInit.BLUE_FESTER_SHROOM_BLOCK.get().asItem());
 		simpleBlockItem(BlockInit.ORANGE_FESTER_SHROOM_BLOCK.get().asItem());
 		simpleBlockItem(BlockInit.CUBED_FUNGUS.get().asItem());
-		simpleBlockItem(BlockInit.FROSTBITTEN_LEAVES.get().asItem());
 		simpleBlockItem(BlockInit.FROSTBITTEN_PLANKS.get().asItem());
 		
 		// Simple 2D Item
