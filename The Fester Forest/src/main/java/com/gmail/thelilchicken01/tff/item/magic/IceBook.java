87f8f6d27d3adc4918e5d98ab102975a7e960605
lilchicky <thelilchicken01@gmail.com>
2023-10-01 05:40:13 -0600

    actually fixed it this time

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/magic/IceBook.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/magic/IceBook.java
@@ -77,7 +77,7 @@ public class IceBook extends Item implements MagicItem, MagicWeapon {
 				
 				FrostBolt shot = bulletItem.createProjectile(player.getLevel(), shotAmmo, player);
 			
-				Vec3 currentPos = player.getEyePosition().add(0.0, 0.1, 0.0);
+				Vec3 currentPos = shot.getPosition(1.0f);
 				Vec3 targetPos = currentEntity.getPosition(1.0f).add(0.0, currentEntity.getEyeHeight() * 0.5, 0.0);
 				Vec3 targetVector = targetPos.subtract(currentPos).normalize();
 			
@@ -105,7 +105,7 @@ public class IceBook extends Item implements MagicItem, MagicWeapon {
 				
 				FrostBolt shot = bulletItem.createProjectile(player.getLevel(), shotAmmo, player);
 			
-				Vec3 currentPos = player.getEyePosition().add(0.0, 0.1, 0.0);
+				Vec3 currentPos = shot.getPosition(1.0f);
 				Vec3 targetPos = nearbyEntities.get(x).getPosition(1.0f).add(0.0, nearbyEntities.get(x).getEyeHeight() * 0.5, 0.0);
 				Vec3 targetVector = targetPos.subtract(currentPos).normalize();
 			
