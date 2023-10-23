b9f590aef669bf2425b52decfad0ed47311a80ea
lilchicky <thelilchicken01@gmail.com>
2023-10-23 00:44:26 -0600

    icicles can be placed on sturdy blocks

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/block/Icicle.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/block/Icicle.java
@@ -3,6 +3,7 @@ package com.gmail.thelilchicken01.tff.block;
 import com.gmail.thelilchicken01.tff.init.BlockInit;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.HangingRootsBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,7 +18,7 @@ public class Icicle extends HangingRootsBlock {
 	public boolean canSurvive(BlockState p_153347_, LevelReader p_153348_, BlockPos p_153349_) {
 		BlockPos blockpos = p_153349_.above();
 		BlockState blockstate = p_153348_.getBlockState(blockpos);
-		return blockstate.is(BlockInit.FROSTBITTEN_LEAVES.get());
+		return blockstate.is(BlockInit.FROSTBITTEN_LEAVES.get()) || blockstate.isFaceSturdy(p_153348_, blockpos, Direction.DOWN);
 	}
 	
 }
