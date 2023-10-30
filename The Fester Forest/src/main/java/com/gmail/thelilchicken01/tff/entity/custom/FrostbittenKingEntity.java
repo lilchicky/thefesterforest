ce31288b85a3634447b78104293b9864ccce83a6
lilchicky <thelilchicken01@gmail.com>
2023-10-29 22:58:18 -0600

    wrong anti cheese direction

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/entity/custom/FrostbittenKingEntity.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/entity/custom/FrostbittenKingEntity.java
@@ -167,7 +167,7 @@ public class FrostbittenKingEntity extends Monster implements IAnimatable {
 			if (this.tickCount % 10 == 0) {
 			
 				for (Player player : nearbyPlayers) {
-					if ((!(player.isSpectator() || player.isCreative()) && ((getY() + 2) <= player.getY() || getY() > player.getY())) && !this.isInvulnerable()) {
+					if ((!(player.isSpectator() || player.isCreative()) && ((getY() + 2) <= player.getY() || getY() > player.getY())) && this.isInvulnerable()) {
 						FrostbittenBolt shot = bulletItem.createProjectile(getLevel(), shotAmmo, this); 
 						
 						shot.setPos(getX(), getY() + getEyeHeight(), getZ());
