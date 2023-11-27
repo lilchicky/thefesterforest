59d6e9660d25b010bf427cda7ce41e9089417f2a
lilchicky <thelilchicken01@gmail.com>
2023-11-26 19:10:16 -0700

    unused code

--- The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/food/GoopySmoothie.java
+++ The Fester Forest/src/main/java/com/gmail/thelilchicken01/tff/item/food/GoopySmoothie.java
@@ -1,24 +1,13 @@
 package com.gmail.thelilchicken01.tff.item.food;
 
-import java.util.List;
-
-import com.gmail.thelilchicken01.tff.TheFesterForest;
 import com.gmail.thelilchicken01.tff.item.item_util.tff_item_overrides.TFFItem;
 
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class GoopySmoothie extends TFFItem {
